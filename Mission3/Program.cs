e9a1e74090f15002f2bd3b978fc773a13a22e6b7
5kiing <103718596+5kiing@users.noreply.github.com>
2023-01-23 17:29:37 -0700

    array in main

--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -4,11 +4,13 @@ namespace Mission3
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
             SupportingTools st = new SupportingTools();
 
-            st.PrintBoard();
+            st.PrintBoard(arr);
 
         }
     }
